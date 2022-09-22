2e3189ce28a1db5e238ebcc48e140cfe54005bbe
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-09-22 09:37:57 +0300

    Delete src directory

--- src/PlaceOrder/constants.ts
+++
@@ -1,2 +0,0 @@
-export const BASE_CURRENCY = "BTC";
-export const QUOTE_CURRENCY = "USDT";
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from "./Button/Button";
-export { NumberInput } from "./NumberInput/NumberInput";
-export { TextInput } from "./TextInput/TextInput";
-export { Switch } from "./Switch/Switch";
-export { TextButton } from "./TextButton/TextButton";
